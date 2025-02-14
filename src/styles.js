74386d5b6fd36047619d10d609563db6e1e306f7
JesseMashoana <jesse.mashoana@gmail.com>
2025-02-14 12:08:22 +0200

    Update styles.js

---
+++ src/styles.js
@@ -0,0 +1,12 @@
+// src/styles.js
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    font-family: 'Arial', sans-serif;
+    background-color: #ffafcc;
+  }
+`;
+
+export default GlobalStyle;
