f05ffb956ea676f543fa01fe5b75a3a06176f75e
JesseMashoana <jesse.mashoana@gmail.com>
2025-02-14 12:06:53 +0200

    Update Gallery.js

---
+++ src/pages/Gallery.js
@@ -0,0 +1,15 @@
+// src/pages/Gallery.js
+import AnimatedPage from "../components/AnimatedPage";
+
+const Gallery = () => {
+  return (
+    <AnimatedPage>
+      <div style={{ textAlign: "center", padding: "50px", color: "white" }}>
+        <h1>Our Beautiful Moments ❤️</h1>
+        <p>(Add an image slider here with your favorite pictures)</p>
+      </div>
+    </AnimatedPage>
+  );
+};
+
+export default Gallery;
