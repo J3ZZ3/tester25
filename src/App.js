7fda2aa9c607a7ae33feeb3ebacd0193940eb8b6
JesseMashoana <jesse.mashoana@gmail.com>
2025-02-14 12:08:07 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { AnimatePresence } from "framer-motion";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Gallery from "./pages/Gallery";
+import LoveLetter from "./pages/LoveLetter";
+import Music from "./pages/Music";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Navbar />
+      <AnimatePresence>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/gallery" element={<Gallery />} />
+          <Route path="/love-letter" element={<LoveLetter />} />
+          <Route path="/music" element={<Music />} />
+        </Routes>
+      </AnimatePresence>
+    </Router>
   );
-}
+};
 
 export default App;
